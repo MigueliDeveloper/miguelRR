1636beff4e011ee448fcba2524e8509266324809
MigueliDeveloper <miguelidevelop@gmail.com>
2023-08-04 17:59:35 +0100

    gffgwerf

---
+++ Actividades/ActividadUF1842/ActividadJQuery2/js/jqueryPractica.js
@@ -0,0 +1,17 @@
+//Si se pone en el script en el header se pone
+$(document).ready(function(){
+    $(".pricinpal").click(function(){
+
+        $(".infoprincipal").css("background-image", "url('media/imagen-curso.jpg')");
+        $(".infoprincipal h1, p").css("color", "#ffff00")
+
+    })
+
+})
+
+
+
+// $("p").click(function(){
+
+//   $(this).hide();
+// });
