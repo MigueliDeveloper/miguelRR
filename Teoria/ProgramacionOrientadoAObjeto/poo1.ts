82b1a18aaca2da04db78848d9630e26589d80d66
MigueliDeveloper <miguelidevelop@gmail.com>
2023-08-11 15:34:02 +0100

    Por mi culpa

---
+++ Teoria/ProgramacionOrientadoAObjeto/poo1.ts
@@ -0,0 +1,21 @@
+
+class Ropa{
+  
+    // Propiedades
+    public color: String;
+    public marca: String;
+    public talla: String;
+    public precio: Number;
+
+    // Método 
+    public setColor(color){
+        this.color = color;
+    }
+
+    public getColor(){
+        return this.color;
+    }
+} 
+
+var camiseta = new Ropa();
+camiseta.setColor("Rojo");
