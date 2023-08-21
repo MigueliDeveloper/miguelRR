a22e607b21414d89c88fca1fe1f74747aedc0070
MigueliDeveloper <miguelidevelop@gmail.com>
2023-08-21 15:56:51 +0100

    holi2

---
+++ Actividades/ActividadesMiguelRamirezRamos/Actividades/2_Actividades-UF-1844/18082023ActividadesAngular/miAngularProyect/src/app/cursos/cursos.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+
+export class CursosComponent {
+
+  public nombre: string
+  public listado: string
+
+  constructor(){
+    this.nombre= "Programación"
+    this.listado= "Listado de Curso"
+
+  }
+
+}
